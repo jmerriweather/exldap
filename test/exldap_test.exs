8003f408d8f3972257ae7fcf4e29992c3c2eb051
Jonathan Merriweather <Jono@merriweather.com.au>
2015-11-12 16:04:20 +1100

    added some tests, test rely on test123 account existing

--- test/exldap_test.exs
+++ test/exldap_test.exs
@@ -2,7 +2,23 @@ defmodule ExldapTest do
   use ExUnit.Case
   doctest Exldap
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "connect to LDAP and get test123 cn attribute" do
+    {:ok, connection} = Exldap.connect
+
+    {:ok, search_result} = Exldap.search_field(connection, "cn", "test123")
+
+    {:ok, first_result} = search_result |> Enum.fetch 0
+
+    object_sid = first_result.attributes['cn']
+
+    assert object_sid == ['test123']
+  end
+
+  test "open LDAP connect and attempt authentication" do
+    {:ok, connection} = Exldap.open
+
+    result = Exldap.verify_credentials(connection, "CN=test123,OU=Accounts,DC=example,DC=com", "PASSWORD")
+
+    assert result == {:error, :invalidCredentials}
   end
 end

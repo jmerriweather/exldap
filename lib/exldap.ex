4b36d3375cbd48544451c08ef21939186a48c3a0
Jonathan Merriweather <Jono@merriweather.com.au>
2015-11-12 12:22:05 +1100

    changed the api, added the ability to open a connection without binding

--- lib/exldap.ex
+++ lib/exldap.ex
@@ -1,6 +1,10 @@
 defmodule Exldap do
 
-  def open_connection do
+  @doc ~S"""
+  Connects to an LDAP server using the settings defined in config.exs
+
+  """
+  def connect do
     settings = Application.get_env :exldap, :settings
 
     server = settings |> Dict.get(:server)
@@ -13,49 +17,90 @@ defmodule Exldap do
   end
 
 
+  @doc ~S"""
+  Connects to an LDAP server using the arguments passed into the function
+
+  """
   def connect(server, port, ssl, user_dn, password) when is_binary(server) do
     connect :erlang.binary_to_list(server), port, ssl, user_dn, password
   end
 
   def connect(server, port, ssl, user_dn, password) do
-    {:ok, connection} = :eldap.open([server], [{:port, port},{:ssl, ssl}])
 
-    bind_connection(connection, user_dn, password)
+    {:ok, connection} = open(server, port, ssl)
 
-    {:ok, connection}
+    case verify_credentials(connection, user_dn, password) do
+      :ok -> {:ok, connection}
+      {_, message} -> {:error, message}
+    end
   end
 
-  defp bind_connection(connection, dn, password) when is_binary(dn) and is_binary(password) do
-    bind_connection connection, :erlang.binary_to_list(dn), :erlang.binary_to_list(password)
+  @doc ~S"""
+  Open a connection to the LDAP server using the settings defined in config.exs
+
+  """
+  def open do
+    settings = Application.get_env :exldap, :settings
+
+    server = settings |> Dict.get(:server)
+    port = settings |> Dict.get(:port)
+    ssl = settings |> Dict.get(:ssl)
+
+    open(server, port, ssl)
   end
 
+  @doc ~S"""
+  Open a connection to the LDAP server
 
-  defp bind_connection(connection, dn, password) do
-    :eldap.simple_bind(connection, dn, password)
+  """
+  def open(server, port, ssl) when is_binary(server) do
+      open(:erlang.binary_to_list(server), port, ssl)
   end
 
-  def search_field(connection, field, name) when is_list(name) do
-    search_field connection, field, :erlang.list_to_binary name
+  def open(server, port, ssl) do
+    :eldap.open([server], [{:port, port},{:ssl, ssl}])
   end
 
+  @doc ~S"""
+  Verify the credentials against a LDAP connection
 
+  """
+  def verify_credentials(connection, user_dn, password) when is_binary(user_dn) and is_binary(password) do
+    verify_credentials connection, :erlang.binary_to_list(user_dn), :erlang.binary_to_list(password)
+  end
+
+  def verify_credentials(connection, user_dn, password) do
+    :eldap.simple_bind(connection, user_dn, password)
+  end
+
+  @doc ~S"""
+  Searches for an LDAP entry, the base dn is obtained from the config.exs
+
+  """
   def search_field(connection, field, name) do
     settings = Application.get_env :exldap, :settings
     base_config = settings |> Dict.get(:base)
+    search_field(connection, base_config, field, name)
+  end
 
+
+  @doc ~S"""
+  Searches for an LDAP entry using the arguments passed into the function
+
+  """
+  def search_field(connection, base, field, name) when is_list(name) do
+    search_field connection, base, field, :erlang.list_to_binary name
+  end
+
+  def search_field(connection, base, field, name) do
     filter = {:filter, :eldap.equalityMatch(field, name)}
-    base = {:base, base_config}
+    base_config = {:base, base}
     scope = {:scope, :eldap.wholeSubtree()}
-    search = [base, scope, filter]
+    search = [base_config, scope, filter]
 
     {:ok, result} = :eldap.search(connection, search)
     result = Exldap.SearchResult.from_record(result)
 
-
-    results = result.entries |> Enum.map(fn(x) -> Exldap.Entry.from_record(x) end)
-
-    {:ok, first} = Enum.fetch(results, 0)
-
-    first.attributes |> Enum.into(Map.new)
+    result.entries |> Enum.map(fn(x) -> Exldap.Entry.from_record(x) end)
   end
 end

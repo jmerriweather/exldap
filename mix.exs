802024bf765e206e0016b9aaa7436bbf13474304
Jonathan Merriweather <Jono@merriweather.com.au>
2016-01-14 12:19:02 +1100

    getting ready to publish to hex.pm

--- mix.exs
+++ mix.exs
@@ -1,13 +1,42 @@
 defmodule Exldap.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+  @url "https://github.com/jmerriweather/exldap"
+  @maintainers ["Jonathan Merriweather"]
+
   def project do
-    [app: :exldap,
-     version: "0.0.2",
+    [name: "Exldap",
+     app: :exldap,
+     version: @version,
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     maintainers: @maintainers,
+     description: "A module for working with LDAP from Elixir",
+     source_url: @url,
+     homepage_url: @url,
+     package: package,
+     deps: deps,
+     docs: docs
+    ]
+  end
+
+  def package do
+    [
+      maintainers: @maintainers,
+      licenses: ["MIT"],
+      links: %{"GitHub" => @url},
+      files: ~w(lib) ++ ~w(LICENSE.md README.md)
+    ]
+  end
+
+  def docs do
+    [
+      extras: ["README.md", "LICENSE.md"],
+      source_ref: "v#{@version}",
+      main: "readme"
+    ]
   end
 
   # Configuration for the OTP application
@@ -27,6 +56,9 @@ defmodule Exldap.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:earmark, ">= 0.0.0", only: :dev},
+      {:ex_doc, "~> 0.11", only: :dev}
+    ]
   end
 end

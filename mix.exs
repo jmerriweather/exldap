7f0fcaaee794dfa92d987f828fa27a99e0d3f28d
Jonathan Merriweather <Jono@merriweather.com.au>
2017-01-19 09:28:33 +1100

    bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Exldap.Mixfile do
   use Mix.Project
 
-  @version "0.3.5"
+  @version "0.3.6"
   @url "https://github.com/jmerriweather/exldap"
   @maintainers ["Jonathan Merriweather"]
 

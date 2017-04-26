3346ca9fe1650d324b0ccd2761d51fc6a37395e9
Jonathan Merriweather <Jono@merriweather.com.au>
2017-04-27 09:02:30 +1000

    update version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Exldap.Mixfile do
   use Mix.Project
 
-  @version "0.4.0"
+  @version "0.4.1"
   @url "https://github.com/jmerriweather/exldap"
   @maintainers ["Jonathan Merriweather"]
 
